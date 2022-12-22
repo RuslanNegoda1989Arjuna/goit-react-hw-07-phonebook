1719a33e2e7b3d16a79fbf64a0d7d10b652f16a3
Ruslan Negoda <iarjuna108@gmail.com>
2022-12-22 21:54:06 +0200

    create

--- src/components/PhonebookForm/PhonebookForm.jsx
+++ src/components/PhonebookForm/PhonebookForm.jsx
@@ -2,18 +2,23 @@ import PropTypes from 'prop-types';
 import { Formik } from 'formik';
 import { Button, FormUs, Input, LabelIn } from './PhonebookForm.styled';
 import { useDispatch, useSelector } from 'react-redux';
-import { getContacts, useGetContactsQuery } from 'redux/sliceContacts';
+import {
+  getContacts,
+  useGetContactsQuery,
+  useCreateContactMutation,
+} from 'redux/sliceContacts';
 import { nanoid } from 'nanoid';
 
 const initialValues = {
   name: '',
-  number: '',
+  phone: '',
 };
 
 export const PhonebookForm = () => {
   const dispatch = useDispatch();
   // для перевірки однакових контактів
   const { data: contacts } = useGetContactsQuery();
+  const [createContact] = useCreateContactMutation();
   // const contacts = useSelector(getContacts);
 
   const userSubmit = (values, { resetForm }) => {
@@ -21,7 +26,8 @@ export const PhonebookForm = () => {
     if (contacts.find(contact => contact.name === values.name)) {
       return alert(`${values.name} is already is contacts`);
     }
-    values.id = nanoid(5);
+    // values.id = nanoid(5);
+    createContact(values);
     //  Записуємо в стейт значення
     // dispatch(addMyContact(values));
     // скидаємо форму
@@ -45,7 +51,7 @@ export const PhonebookForm = () => {
             Number
             <Input
               type="tel"
-              name="number"
+              name="phone"
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
